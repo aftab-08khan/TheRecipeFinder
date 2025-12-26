8a52b57f08cdc5240f1608d229e31ef17dd4f121
aftab-08khan <akkhanaftab08@gmail.com>
2025-12-26 16:59:42 +0400

    UI | Navbar Menu Links Color Changed

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -13,7 +13,7 @@ const CustomButton = ({
   const navigate = useNavigate();
 
   const baseClasses =
-    "inline-flex items-center justify-center gap-2 text-captialize transition-all duration-300 ease-in-out transform focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-opacity-50";
+    "inline-flex items-center justify-center gap-2 text-captialize transition-all duration-300 ease-in-out transform focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-opacity-50 mb-3";
 
   const sizeClasses =
     "text-sm md:text-base px-4 py-2 md:px-6 md:py-3 rounded-full";
